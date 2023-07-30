a015d3784b32a85871b80c6a48c02dcf1cb24177
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-07-29 21:20:48 -0300

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by Gabriel Moreira on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
