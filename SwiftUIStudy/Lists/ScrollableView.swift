2399a034f0e2f91fddf7ef32aed8c0a43de2a498
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-08-19 01:18:44 -0300

    Added scrollable list view

---
+++ SwiftUIStudy/Lists/ScrollableView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct ScrollableView: View {
+    let guides = [
+        GridItem(.fixed(75)),
+        GridItem(.fixed(75)),
+        GridItem(.fixed(75))
+    ]
+
+    @ViewBuilder
+    var carousel: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(1...10, id: \.self) { value in
+                    VStack {
+                        Text("Value")
+                        Text("\(value)")
+                            .font(.system(size: 30))
+                            .fontWeight(.bold)
+                    }
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    var gridView: some View {
+        LazyVGrid(columns: guides) {
+            ForEach(1...12, id: \.self) { value in
+                Text("\(value)")
+                    .font(.system(size: 24))
+            }
+        }
+    }
+
+    var body: some View {
+        ScrollViewReader { proxy in
+            ScrollView {
+                LazyVStack {
+                    ForEach(0...5, id: \.self) { value in
+                        VStack {
+                            Text("Value: \(value)")
+                            Divider()
+                        }
+                    }
+                    carousel.padding(16)
+                    gridView
+                    Button("Go back") {
+                        proxy.scrollTo(0, anchor: .top)
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct ListsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollableView()
+    }
+}
